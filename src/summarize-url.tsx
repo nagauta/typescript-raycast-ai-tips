ea09d0a5f4e6ebb67f07e41b8f91f971e7da021c
mie035 <hello@michitoshiyano.com>
2024-10-04 21:58:50 +0900

    add command

--- src/summarize-url.tsx
+++ src/summarize-url.tsx
@@ -1,7 +1,22 @@
-import { Detail } from "@raycast/api";
-import { main } from "../chrome-url-fetcher/src/main";
-
+import { Detail, getPreferenceValues } from "@raycast/api";
+import { getChromeUrl, fethInformationFromUrl, summarizer } from "../chrome-url-fetcher/src/lib";
+import { useEffect, useState } from "react";
+export interface Preferences {
+  OPENAI_API_KEY: string;
+}
 export default function Command() {
-  main();
-  return <Detail markdown="# Hello World" />;
+  const [summary, setSummary] = useState<string | null>(null);
+  const { OPENAI_API_KEY }: Preferences = getPreferenceValues();
+  const summarize = async () => {
+    const url: string = await getChromeUrl();
+    const info: string = await fethInformationFromUrl(url);
+    const ai = await summarizer(OPENAI_API_KEY);
+    setSummary(await ai(info));
+  };
+
+  useEffect(() => {
+    summarize();
+  }, []);
+
+  return <Detail markdown={`${summary ?? "now loading..."}`} />;
 }
